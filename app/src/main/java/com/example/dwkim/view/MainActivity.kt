b5fd5f164decaccd71a945127ae930fbf61bca98
dwkim <dowoo.kim.891220@gmail.com>
2019-04-04 23:13:20 +0900

    temp commit

--- app/src/main/java/com/example/dwkim/view/MainActivity.kt
+++ app/src/main/java/com/example/dwkim/view/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.dwkim.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dwkim.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragments()
+    }
+
+    private fun initFragments(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.fl_aMain_container,
+                fragment,
+                tag
+            )
+            .commit()
     }
 }
